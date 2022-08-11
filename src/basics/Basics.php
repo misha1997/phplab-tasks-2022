1af69bbb9592dc1abfde205bf298b6a349c64b75
misha1997 <mishaotroshenko2013@gmail.com>
2022-08-12 00:04:49 +0300

    |Basic| use of psr-4 standard

--- src/basics/Basics.php
+++ src/basics/Basics.php
@@ -2,33 +2,35 @@
 
 namespace basics;
 
-class Basics implements BasicsInterface 
+class Basics implements BasicsInterface
 {
-  private const MIN_TIME = 15;
-  private const TIME_TITLE = ['first', 'second', 'third', 'fourth'];
+    private const MIN_TIME = 15;
+    private const TIME_TITLE = ['first', 'second', 'third', 'fourth'];
 
-  private BasicsValidator $validator;
+    private BasicsValidator $validator;
 
-  public function __construct(BasicsValidator $validator)
-  {
-    $this->validator = $validator;
-  }
+    public function __construct(BasicsValidator $validator)
+    {
+        $this->validator = $validator;
+    }
 
-  public function getMinuteQuarter(int $minute): string
-  {
-    $this->validator->isMinutesException($minute);
-    return self::TIME_TITLE[$minute ? ceil($minute / (self::MIN_TIME)) - 1 : 3];
-  }
+    public function getMinuteQuarter(int $minute): string
+    {
+        $this->validator->isMinutesException($minute);
+        return self::TIME_TITLE[$minute ? ceil($minute / (self::MIN_TIME)) - 1 : 3];
+    }
 
-  public function isLeapYear(int $year): bool
-  {
-    $this->validator->isYearException($year);
-    return (!($year % 4) && ($year % 100)) || !($year % 400);
-  }
+    public function isLeapYear(int $year): bool
+    {
+        $this->validator->isYearException($year);
+        return (!($year % 4) && ($year % 100)) || !($year % 400);
+    }
 
-  public function isSumEqual(string $input): bool
-  {
-    $this->validator->isValidStringException($input);
-    return array_sum(str_split(substr($input, 0, 3))) == array_sum(str_split(substr($input, 3, 6)));
-  }
+    public function isSumEqual(string $input): bool
+    {
+        $this->validator->isValidStringException($input);
+        $firstPartInput = str_split(substr($input, 0, 3));
+        $secondPartInput = str_split(substr($input, 3, 6));
+        return array_sum($firstPartInput) == array_sum($secondPartInput);
+    }
 }
--- src/basics/BasicsValidator.php
+++ src/basics/BasicsValidator.php
@@ -10,21 +10,21 @@ class BasicsValidator implements BasicsValidatorInterface
   public function isMinutesException(int $minute): void
   {
     if($minute < 0 || $minute > 60) {
-      throw new InvalidArgumentException('error');
+      throw new InvalidArgumentException('Interval must be between 0 and 60');
     }
   }
 
   public function isYearException(int $year): void
   {
     if($year < 1900) {
-      throw new InvalidArgumentException('error');
+      throw new InvalidArgumentException('Year less than 1900');
     }
   }
 
   public function isValidStringException(string $input): void
   {
     if(strlen($input) > 6 || strlen($input) < 6) {
-      throw new InvalidArgumentException('error');
+      throw new InvalidArgumentException('Value must not be greater or less than 6');
     }
   }
 }
